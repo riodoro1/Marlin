374d68d741d6a9aeaeee0c25151274d4dd1c8feb
Rafal Bialek <bialek.rafal@gmail.com>
2018-11-01 10:49:02 +0100

    New steps/mm value for extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -617,7 +617,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80, 80, 1600, 87.45 /*~(3200 / 37.7)*/}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80, 80, 1600, 87.97}
 
 /**
  * Default Max Feed Rate (mm/s)
